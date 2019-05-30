8a22b15d11836985a0c10d7e860d1192d99e2a98
gentksb <gen@gensobunya.net>
2019-05-30 15:31:29 +0900

    end of regular hands-on menu

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Table from './views/Table.vue';
+import Form from './views/Form.vue';
 
 Vue.use(Router)
 
@@ -12,6 +14,16 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/table',
+      name: 'table',
+      component: Table
+    },
+    {
+      path: '/form',
+      name: 'form',
+      component: Form
     },
     {
       path: '/about',
